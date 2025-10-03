06dfaee97b04ea59cc643020e417182d890f6364
Alex-brh <133638105+Alex-brh@users.noreply.github.com>
2025-10-03 14:14:13 -0600

    More tests and clean-up.

--- tests/POM/online-store-home.ts
+++ tests/POM/online-store-home.ts
@@ -1,20 +1,20 @@
-import { type Page, type Locator, expect, APIRequestContext } from '@playwright/test';
-import { url } from 'inspector';
+import {type Page,type Locator, expect, APIRequestContext} from "@playwright/test";
+import { url } from "inspector";
 
 export class StoreHomePage {
-readonly homeMenuItem: Locator;
-readonly storemenuItem: Locator;
-readonly faqMenuItem: Locator;
-readonly customerTestimonialsMenuItem: Locator;
-readonly contactMenuItem: Locator;
-elementsWithFramesMenuItem: Locator;
-readonly basketMenuItem: Locator;
-readonly aboutUsMenuItem: Locator;
-private readonly pageHeader: Locator;
-readonly shopNowButton: Locator;
-private readonly welcomeSubHeader: Locator;
+  readonly homeMenuItem: Locator;
+  readonly storemenuItem: Locator;
+  readonly faqMenuItem: Locator;
+  readonly customerTestimonialsMenuItem: Locator;
+  readonly contactMenuItem: Locator;
+  elementsWithFramesMenuItem: Locator;
+  readonly basketMenuItem: Locator;
+  readonly aboutUsMenuItem: Locator;
+  private readonly pageHeader: Locator;
+  readonly shopNowButton: Locator;
+  private readonly welcomeSubHeader: Locator;
 
-constructor(public readonly page: Page) {
+  constructor(public readonly page: Page) {
     this.homeMenuItem = this.page.locator('a[class^="jw-menu-link"][href="/"]');
     this.storemenuItem = this.page.locator('a[class^="jw-menu-link"][href="/store"] > span');
     this.faqMenuItem = this.page.locator('a[class^="jw-menu-link"][href="/faq"] > span');
@@ -25,20 +25,34 @@ constructor(public readonly page: Page) {
     this.aboutUsMenuItem = this.page.locator('a[class^="jw-menu-link"][href="/contact"] > span');
     this.pageHeader = this.page.locator('div[class="jw-slideshow-title"]');
     this.shopNowButton = this.page.locator('a[href="/store"] > div');
-    this.welcomeSubHeader = this.page.locator('h1[class^="jw-heading"]').filter({ hasText: `Welcome to Alex's test automation site for practice` });;
-}
+    this.welcomeSubHeader = this.page.locator('h1[class^="jw-heading"]').filter({hasText: `Welcome to Alex's test automation site for practice`});
+  }
 
-    async gotoStoreHomePage(page: Page, request: APIRequestContext) {
-        const url = 'https://free-5288352.webadorsite.com/';
-        const response = await request.get(url);
-        await expect(response).toBeOK();
-        await page.goto(url, { waitUntil: 'commit' });
-        await page.waitForURL(url, { timeout: 15000 });
-        await expect(this.homeMenuItem).toBeVisible();
-        await expect(this.homeMenuItem).toHaveText(" Home ");
-    }
-
-async validateAllMenuItemsPresence() {
+  /**
+   * Navigates to the store's home page, validates the initial API call,
+   * and asserts the presence of the Home menu item.
+   * This method handles common navigation and initial page state checks.
+   *
+   * @param {Page} page The Playwright Page object.
+   * @param {APIRequestContext} request The Playwright APIRequestContext fixture.
+   */
+  async gotoStoreHomePage(page: Page, request: APIRequestContext) {
+    const url = "https://free-5288352.webadorsite.com/";
+    const response = await request.get(url);
+    await expect(response).toBeOK();
+    await page.goto(url, { waitUntil: "commit" });
+    await page.waitForURL(url, { timeout: 15000 });
+    await expect(this.homeMenuItem).toBeVisible();
+    await expect(this.homeMenuItem).toHaveText(" Home ");
+  }
+  /**
+   * Validates the visibility of all menu items on the navigation bar.
+   * This method contains a series of assertions to ensure each menu item is present
+   * and visible to the user.
+   *
+   * @param {void} - No parameters are required as the method uses class properties.
+   */
+  async validateAllMenuItemsPresence() {
     await expect(this.homeMenuItem).toBeVisible();
     await expect(this.storemenuItem).toBeVisible();
     await expect(this.faqMenuItem).toBeVisible();
@@ -47,12 +61,33 @@ async validateAllMenuItemsPresence() {
     await expect(this.elementsWithFramesMenuItem).toBeVisible();
     await expect(this.basketMenuItem).toBeVisible();
     await expect(this.aboutUsMenuItem).toBeVisible();
-}
+  }
 
+  /**
+   * Clicks a specified locator and waits for the page URL to change.
+   * This method handles the common pattern of clicking a link that triggers a navigation.
+   *
+   * @param {Page} page The Playwright Page object.
+   * @param {Locator} locator The Locator for the element to be clicked.
+   * @param {string} url The expected URL string or URL pattern after the click.
+   */
   async clickMenuItem(page: Page, locator: Locator, url: string) {
     await expect(locator).toBeVisible();
     locator.click();
     await expect(page).toHaveURL(url);
-}
+  }
 
+  /**
+   * Clicks a "Shop now" button at a specified index.
+   * The method uses the locator's nth() method to target a specific button
+   * from a list of elements with the same selector.
+   *
+   * @param {Page} page The Playwright Page object.
+   * @param {number} buttonIndex The zero-based index of the button to click.
+   */
+  async clickShopNowButton(page: Page, buttonIndex: number) {
+    const locator = this.shopNowButton.nth(buttonIndex);
+    await expect(locator).toBeVisible();
+    locator.click();
+  }
 }
--- tests/online-store-home.spec.ts
+++ tests/online-store-home.spec.ts
@@ -1,24 +1,24 @@
 import { test, expect } from '@playwright/test';
-import { StoreHomePage } from '../tests/POM/online-store-home';
+import { StoreHomePage } from './POM/online-store-home';
 
 const baseUrl = "https://free-5288352.webadorsite.com/";
 
-test.describe(`Validate online store Home page by`, () => {
+test.describe(`Test online store 'Home' page by`, () => {
   let storeHomePage: StoreHomePage;
 
   // Request both `page` and `request` fixtures here
   test.beforeEach(async ({ page, request }) => {
     storeHomePage = new StoreHomePage(page);
-    // You now have access to both `page` and `request`
-    // and can pass them to the Page Object method.
     await storeHomePage.gotoStoreHomePage(page, request);
   });
 
   test('ensuring that all menu items show up', async ({ page }) => {
+    // Validate the presence of all menu items on the 'Home' page.
     await storeHomePage.validateAllMenuItemsPresence();
   });
 
   test('switching over between menus and verifying page URL', async ({ page }) => {
+    // Click each one of the menu items on the 'Home' page and validate the page URL each time.
     await storeHomePage.clickMenuItem(page, storeHomePage.storemenuItem, `${baseUrl}store`);
     await storeHomePage.clickMenuItem(page, storeHomePage.homeMenuItem, `${baseUrl}`);
     await storeHomePage.clickMenuItem(page, storeHomePage.faqMenuItem, `${baseUrl}faq`);
@@ -26,5 +26,15 @@ test.describe(`Validate online store Home page by`, () => {
     await storeHomePage.clickMenuItem(page, storeHomePage.contactMenuItem, `${baseUrl}contact`);
     await storeHomePage.clickMenuItem(page, storeHomePage.elementsWithFramesMenuItem, `${baseUrl}elements-with-frames`);
     await storeHomePage.clickMenuItem(page, storeHomePage.aboutUsMenuItem, `${baseUrl}contact`);
+  });
+
+  test(`clicking each one of 'Shop now' buttons and verifying page url it leads to`, async ({ page }) => {
+    // Click each one of the 3 'Shop now' buttons on the 'Home' page and validate the page URL.
+    for (let i = 0; i < 3; i++) {
+      await storeHomePage.clickShopNowButton(page, i);
+      await expect(page).toHaveURL(`${baseUrl}store`);
+      await page.goBack();
+      await expect(page).toHaveURL(baseUrl, { timeout: 15000 });
+    }
   });
 });
