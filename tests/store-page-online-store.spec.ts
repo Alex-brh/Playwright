32ea905cdc57974acea48054f728d36a2609b0f4
Alex-brh <133638105+Alex-brh@users.noreply.github.com>
2025-10-08 11:38:36 -0600

    Added more test data to the productDetails object.

--- tests/store-page-online-store.spec.ts
+++ tests/store-page-online-store.spec.ts
@@ -7,25 +7,36 @@ import { url } from 'inspector';
 const baseURL = config.use?.baseURL ?? 'https://free-5288352.webadorsite.com/';
 
 test.describe(`Test online store 'Store' page by`, () => {
-  let storeHomePage: StoreHomePage, storePage: StorePage;
+    let storeHomePage: StoreHomePage, storePage: StorePage;
 
-  // Request both `page` and `request` fixtures here
-  test.beforeEach(async ({ page, request }) => {
-    storeHomePage = new StoreHomePage(page);
-    storePage = new StorePage(page);
-    await storeHomePage.gotoStoreHomePage(page, request);
-  });
+    // Request both `page` and `request` fixtures here
+    test.beforeEach(async ({ page, request }) => {
+        storeHomePage = new StoreHomePage(page);
+        storePage = new StorePage(page);
+        await storeHomePage.gotoStoreHomePage(page, request);
+    });
 
-  test(`navigating to the 'Store' page and validating product headers`, async ({ page }) => {
-    await storeHomePage.clickMenuItem(page, storeHomePage.storMenuItem, `${baseURL}store`);
-    await expect(page).toHaveURL(`${baseURL}store`);
-    const productDetails = [{
-    productIndex: 1,
-    productHref: "/product/15278051/best-test-script-b", // Optional.
-    headerText: "Best test script B" // Optional.
-    }];
-    for (const product of productDetails) {
-        await storePage.validateProductHeader(page, product )
-    }
-  });
+    test(`navigating to the 'Store' page and validating product headers`, async ({ page }) => {
+        await storeHomePage.clickMenuItem(page, storeHomePage.storMenuItem, `${baseURL}store`);
+        await expect(page).toHaveURL(`${baseURL}store`);
+        const productDetails = [
+            {
+                productIndex: 0,
+                productHref: "/product/15277987/best-test-script-a", // Optional.
+                headerText: "Best test script A" // Optional.
+            },
+            {
+                productIndex: 1,
+                productHref: "/product/15278051/best-test-script-b", // Optional.
+                headerText: "Best test script B" // Optional.
+            },
+            {
+                productIndex: 2,
+                productHref: "/product/15278052/best-test-script-c", // Optional.
+                headerText: "Best test script C" // Optional.
+            },];
+        for (const product of productDetails) {
+            await storePage.validateProductHeader(page, product)
+        }
+    });
 });
