55d3ef1554cb21df31f96e82b22b156ca30d93cd
Alex-brh <133638105+Alex-brh@users.noreply.github.com>
2025-10-10 14:23:11 -0600

    Clean-up.

--- tests/customer-testimonial.spec.ts
+++ tests/customer-testimonial.spec.ts
@@ -6,12 +6,12 @@ import config from '../playwright.config';
 // Determine the base URL from the Playwright configuration or use a fallback URL.
 const baseURL = config.use?.baseURL ?? 'https://free-5288352.webadorsite.com/';
 
-// Use `test.describe` to group related test cases for the "Store" page.
+// Group related test cases for the "Store" page.
 test.describe(`Test online store 'Customer Testimonials' page by`, () => {
     // Declare variables for the Page Object Model (POM) classes to be used in the tests.
     let storeHomePage: StoreHomePage, customerTestimonials: CustomerTestimonials;
 
-    // The `beforeEach` hook runs before every test in this `test.describe` block.
+    // Run before every test in this `test.describe` block.
     test.beforeEach(async ({ page, request }) => {
         // Initialize the POM classes with the Playwright `page` fixture.
         storeHomePage = new StoreHomePage(page);
@@ -20,8 +20,7 @@ test.describe(`Test online store 'Customer Testimonials' page by`, () => {
         // Navigate to the store's home page using a POM method.
         await storeHomePage.gotoStoreHomePage(page, request);
 
-        // Click the 'Customer Testimonials' menu item to navigate to the 'FAQ' page.
-        // The POM method `clickMenuItem` also waits for navigation to complete.
+        // Click the 'Customer Testimonials' menu item to navigate to the 'Customer Testimonials' page.
         await storeHomePage.clickMenuItem(page, storeHomePage.customerTestimonialsMenuItem, `${baseURL}customer-testimonials`);
 
         // Assert that the navigation to the 'Customer Testimonials' page was successful by checking the URL.
