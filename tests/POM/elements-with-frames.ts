6cb8f865ff537130292e15519afc020243d1fc23
Alex-TDS\Alex <alex.brohin@everflowutilities.com>
2026-02-04 13:16:30 -0700

    Updated tests\POM\elements-with-frames.ts

--- tests/POM/elements-with-frames.ts
+++ tests/POM/elements-with-frames.ts
@@ -12,11 +12,24 @@ export class ElementsWithFramesPage {
     this.allFrames = this.page.locator('div[class="iframely-embed"] > div');
     this.firstFrameBody = this.page.locator('div[class="iframely-embed"]').nth(0).locator('div > div[class="body"]').nth(0);
   }
+  /**
+   * @description Validate the iframe count on the page.
+   * @param expectedCount 
+   * @returns void
+   * @example
+   * await elementsWithFramesPage.validateIframeCount(3);
+   */
   async validateIframeCount(expectedCount = 0) {
     const count = await this.allFrames.count();
     expect(count).toEqual(expectedCount);
   }
 
+  /**
+   * @description Validate the first frame has content.
+   * @returns void
+   * @example
+   * await elementsWithFramesPage.validateFirstFrameHasContent();
+   */
   async validateFirstFrameHasContent() {
     await expect(this.firstFrameBody).toBeAttached();
     const bodyText = await this.firstFrameBody.innerText();
